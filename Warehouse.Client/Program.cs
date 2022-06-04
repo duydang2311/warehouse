b844877e91a27906037746b9f2a2fcbf5030ef40
duydang2311 <duydang2412@gmail.com>
2022-06-04 15:28:31 +0700

    feat(Program): inject dependencies

--- Warehouse.Client/Program.cs
+++ Warehouse.Client/Program.cs
@@ -1,14 +1,25 @@
-﻿using System.Net;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using Warehouse.Client.SocketClients;
+using Warehouse.Shared.BinaryHelpers;
 
 namespace Warehouse.Client;
 
 public class Program
 {
-	public static void Main()
-	{
-		new SocketClient();
-	}
+    public static ServiceProvider Provider { get; protected set; } = null!;
+
+    public static void Main()
+    {
+        var services = new ServiceCollection();
+        services.WithBinaryHelpers().WithSocketClients();
+        Provider = services.BuildServiceProvider();
+
+        var client = Provider.GetRequiredService<ISocketClient>();
+        client.Connect("localhost", 4242);
+        for (; ; )
+            ;
+    }
 }
