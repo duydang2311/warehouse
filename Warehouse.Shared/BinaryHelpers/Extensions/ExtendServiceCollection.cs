fbfb70bc750aeeed4be73b40b4e59fb3453053e0
duydang2311 <duydang2412@gmail.com>
2022-06-06 01:16:46 +0700

    refactor(Shared.BinaryHelpers): make ExtendServiceCollection partial

--- Warehouse.Shared/BinaryHelpers/Extensions/ExtendServiceCollection.cs
+++ Warehouse.Shared/BinaryHelpers/Extensions/ExtendServiceCollection.cs
@@ -2,13 +2,11 @@ using Warehouse.Shared.BinaryHelpers;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
-public static class ExtendServiceCollection
+public static partial class ExtendServiceCollection
 {
     public static IServiceCollection WithBinaryHelpers(this IServiceCollection self)
     {
-        self
-            .AddSingleton<IBinaryHelper, BinaryHelper>()
-            .AddSingleton<BinaryHelperService>();
+        self.AddSingleton<IBinaryHelper, BinaryHelper>().AddSingleton<BinaryHelperService>();
         return self;
     }
 }
