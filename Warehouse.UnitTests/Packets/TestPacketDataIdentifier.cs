1830cf2b6736841c59ccba96a135946c0f251183
duydang2311 <duydang2412@gmail.com>
2022-06-07 17:03:33 +0700

    refactor(TestPacketDataIdentifier): update tests

--- Warehouse.UnitTests/Packets/TestPacketDataIdentifier.cs
+++ Warehouse.UnitTests/Packets/TestPacketDataIdentifier.cs
@@ -16,12 +16,20 @@ public class TestPacketDataIdentifier
     }
 
     [Fact]
-    public void Serialize_Deserialize_Should_Equal()
+    public void Identity_Not_Equal_0()
     {
         var data = new ClientPacketData(ClientPacketType.Select);
         var packet = serializer.TrySerialize<IClientPacketData>(data);
         Assert.NotNull(packet);
         Assert.NotEqual((ulong)0, identifier.TryIdentify<IClientPacketData>());
+    }
+
+    [Fact]
+    public void Serialized_Identity_Must_Equal()
+    {
+        var data = new ClientPacketData(ClientPacketType.Select);
+        var packet = serializer.TrySerialize<IClientPacketData>(data);
+        Assert.NotNull(packet);
         Assert.Equal(packet!.Identity, identifier.TryIdentify<IClientPacketData>());
     }
 }
