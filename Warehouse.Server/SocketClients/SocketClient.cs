97246a8a9aab7b4f80250267b621d5720a8706d8
duydang2311 <duydang2412@gmail.com>
2022-06-07 20:42:20 +0700

    feat(SocketClient): inject IPacketDataSerializer

--- Warehouse.Server/SocketClients/SocketClient.cs
+++ Warehouse.Server/SocketClients/SocketClient.cs
@@ -1,8 +1,15 @@
 using Warehouse.Shared.Sockets;
+using Warehouse.Shared.Packets.Serializers;
 
 namespace Warehouse.Server.SocketClients;
 
 public partial class SocketClient : Socket, ISocketClient
 {
-    public SocketClient(System.Net.Sockets.Socket socket) : base(socket) { }
+    private readonly IPacketDataSerializer serializer;
+
+    public SocketClient(IPacketDataSerializer serializer, System.Net.Sockets.Socket socket)
+        : base(socket)
+    {
+        this.serializer = serializer;
+    }
 }
--- Warehouse.Server/SocketClients/SocketClientFactory.cs
+++ Warehouse.Server/SocketClients/SocketClientFactory.cs
@@ -1,13 +1,23 @@
 using System.Net.Sockets;
+using Warehouse.Shared.Packets.Serializers;
 
 namespace Warehouse.Server.SocketClients;
 
 public class SocketClientFactory : ISocketClientFactory
 {
-    public ISocketClient GetService(System.Net.Sockets.Socket socket) => new SocketClient(socket);
+    private readonly IPacketDataSerializer serializer;
+
+    public SocketClientFactory(IPacketDataSerializer serializer)
+    {
+        this.serializer = serializer;
+    }
+
+    public ISocketClient GetService(System.Net.Sockets.Socket socket) =>
+        new SocketClient(serializer, socket);
 
     public ISocketClient GetService() =>
         new SocketClient(
+            serializer,
             new System.Net.Sockets.Socket(
                 AddressFamily.InterNetworkV6,
                 SocketType.Stream,
