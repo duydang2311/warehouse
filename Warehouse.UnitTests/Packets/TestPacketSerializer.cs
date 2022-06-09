82cf1eea226b6fd115a72f01258a32c8a3bf9075
duydang2311 <duydang2412@gmail.com>
2022-06-09 23:00:43 +0700

    feat(TestPacketSerializer): test async deserialize

--- Warehouse.UnitTests/Packets/TestPacketSerializer.cs
+++ Warehouse.UnitTests/Packets/TestPacketSerializer.cs
@@ -1,8 +1,21 @@
 using Warehouse.Shared.Packets;
 using Warehouse.Shared.Packets.Identifiers;
 using Warehouse.Shared.Packets.Serializers;
+using MessagePack;
 
 namespace Warehouse.UnitTests.Packets;
+
+[Union(0, typeof(TestPacket))]
+public interface ITestPacket : IPacket
+{
+    string Name { get; set; }
+}
+
+[MessagePackObject(true)]
+public class TestPacket : ITestPacket
+{
+    public string Name { get; set; } = "Test packet";
+}
 
 public class TestPacketSerializer
 {
@@ -24,5 +37,18 @@ public class TestPacketSerializer
         var deserialized = serializer.TryDeserialize<IClientPacket>(packet!);
         Assert.NotNull(deserialized);
         Assert.Equal(data.Type, deserialized!.Type);
+    }
+
+    [Fact]
+    public async Task Deserialize_To_IPacket_Type_Async()
+    {
+        ITestPacket packet = new TestPacket();
+        var header = await serializer.TrySerializeAsync<ITestPacket>(packet);
+        Assert.NotNull(header);
+        using var stream = await serializer.TrySerializeAsync(header!);
+        Assert.NotNull(stream);
+        var deserializedPacket = await serializer.TryDeserializeAsync<ITestPacket>(stream!);
+        Assert.NotNull(deserializedPacket);
+        Assert.Equal(packet!.Name, deserializedPacket!.Name);
     }
 }
