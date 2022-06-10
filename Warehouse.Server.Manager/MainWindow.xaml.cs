d1fcb8ff7e4db7f0a50b9e123380fc066234a7e8
duydang2412 <duydang2412@gmail.com>
2022-06-11 00:32:47 +0700

    feat(MainWindow): use IMainWindowViewModel - use custom title bar

--- Warehouse.Server.Manager/MainWindow.xaml.cs
+++ Warehouse.Server.Manager/MainWindow.xaml.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.UI.Xaml;
 using Warehouse.Server.Manager.Views;
+using Warehouse.Server.Manager.ViewModels;
 
 namespace Warehouse.Server.Manager;
 public sealed partial class MainWindow : Window
 {
-	public MainWindow(HomePage page)
-	{
-		InitializeComponent();
-		Title = "Warehouse Manager";
-		Content = page;
-	}
+    public MainWindow(IMainWindowViewModel viewModel, AuthenticationPage page)
+    {
+        InitializeComponent();
+        ExtendsContentIntoTitleBar = true;
+        SetTitleBar(TitleBar);
+        Frame.DataContext = viewModel;
+        viewModel.Content = page;
+    }
 }
