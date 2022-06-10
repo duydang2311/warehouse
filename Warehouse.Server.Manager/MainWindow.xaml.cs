a0ed1dd61e1e44f46c318cd7c2f2cff8c54f27fb
duydang2412 <duydang2412@gmail.com>
2022-06-11 01:13:23 +0700

    feat(AuthenticationPage): add authentication form

--- Warehouse.Server.Manager/MainWindow.xaml.cs
+++ Warehouse.Server.Manager/MainWindow.xaml.cs
@@ -1,16 +1,18 @@
 ﻿using Microsoft.UI.Xaml;
 using Warehouse.Server.Manager.Views;
 using Warehouse.Server.Manager.ViewModels;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Warehouse.Server.Manager;
 public sealed partial class MainWindow : Window
 {
-    public MainWindow(IMainWindowViewModel viewModel, AuthenticationPage page)
+    public MainWindow(IMainWindowViewModel viewModel, AuthenticationPage page, HomePage page2)
     {
         InitializeComponent();
         ExtendsContentIntoTitleBar = true;
         SetTitleBar(TitleBar);
-        Frame.DataContext = viewModel;
         viewModel.Content = page;
+        Frame.DataContext = viewModel;
     }
 }
