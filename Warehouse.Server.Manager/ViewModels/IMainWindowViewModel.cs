ce581f57b035ecb341ab4da82ec3cb6ef275463d
duydang2412 <duydang2412@gmail.com>
2022-06-11 00:32:07 +0700

    feat(ViewModels): implement IMainWindowViewModel

---
+++ Warehouse.Server.Manager/ViewModels/IMainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace Warehouse.Server.Manager.ViewModels;
+
+public interface IMainWindowViewModel
+{
+    Page Content { get; set; }
+}
---
+++ Warehouse.Server.Manager/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace Warehouse.Server.Manager.ViewModels;
+
+public class MainWindowViewModel : ObservableObject, IMainWindowViewModel
+{
+    private Page content;
+    public Page Content
+    {
+        get => content;
+        set => SetProperty(ref content, value);
+    }
+}
