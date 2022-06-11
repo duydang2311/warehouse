bdb208d6647f26ba64d5e169abd297191e04698a
duydang2412 <duydang2412@gmail.com>
2022-06-11 14:48:33 +0700

    feat(Program): use Authenticate() - split Listener to files

---
+++ Warehouse.Server/Program.Listener.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Warehouse.Server.SocketListeners;
+using Warehouse.Server.SocketHandlers;
+using Warehouse.Shared.Packets;
+using Warehouse.Shared.Packets.Identifiers;
+
+namespace Warehouse.Server;
+
+public partial class Program
+{
+	private static void CreateListener()
+	{
+		var listener = Provider.GetRequiredService<ISocketListenerFactory>().GetService();
+		listener.Bind("localhost", 4242);
+		listener.BeginAccept();
+		listener.Accepted += Listener_Accepted;
+		Console.WriteLine("Listening on localhost:4242");
+	}
+
+	private static void Listener_Accepted(ISocketListener sender, ISocketHandler handler)
+	{
+		handler.BeginReceive();
+		handler.Disconnecting += Client_Disconnecting;
+		handler.Received += Client_Received;
+		Console.WriteLine($"{handler.RemoteEndPoint} connected");
+	}
+
+	private static void Client_Disconnecting(ISocketHandler client)
+	{
+		Console.WriteLine($"Disposing handler {client.RemoteEndPoint} due to disconnected");
+		client.Dispose();
+	}
+
+	private static void Client_Received(ISocketHandler client, IPacketHeader packet)
+	{
+		Console.Write(
+			$"Packet {packet.Identity} has {packet.Buffer.Length} bytes: {string.Join(' ', packet.Buffer)}"
+		);
+	}
+}
--- Warehouse.Server/Program.cs
+++ Warehouse.Server/Program.cs
@@ -18,9 +18,12 @@ public partial class Program
 			.WithSocketListeners()
 			.WithSocketHandlers()
 			.WithSockets()
-			.WithPackets();
+			.WithPackets()
+			.WithDatabases();
 		Provider = services.BuildServiceProvider();
 		services.AddSingleton<IServiceProvider, ServiceProvider>(p => Provider);
+		Authenticate();
+
 		var identifier = Provider.GetRequiredService<IPacketIdentifier>();
 		identifier.Register(Assembly.GetExecutingAssembly());
 		foreach (var i in Assembly.GetExecutingAssembly().GetReferencedAssemblies())
@@ -31,32 +34,7 @@ public partial class Program
 				break;
 			}
 		}
-
-		var listener = Provider.GetRequiredService<ISocketListenerFactory>().GetService();
-		listener.Bind("localhost", 4242);
-		listener.BeginAccept();
-		listener.Accepted += Listener_Accepted;
+		CreateListener();
 		ReadCommand();
-	}
-
-	private static void Listener_Accepted(ISocketListener sender, ISocketHandler handler)
-	{
-		handler.BeginReceive();
-		handler.Disconnecting += Client_Disconnecting;
-		handler.Received += Client_Received;
-		Console.WriteLine($"{handler.RemoteEndPoint} connected");
-	}
-
-	private static void Client_Disconnecting(ISocketHandler client)
-	{
-		Console.WriteLine($"Disposing handler {client.RemoteEndPoint} due to disconnected");
-		client.Dispose();
-	}
-
-	private static void Client_Received(ISocketHandler client, IPacketHeader packet)
-	{
-		Console.Write(
-			$"Packet {packet.Identity} has {packet.Buffer.Length} bytes: {string.Join(' ', packet.Buffer)}"
-		);
 	}
 }
