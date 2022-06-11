0c41c07349fa817f0810a9c23d4862e3c0aee4ea
duydang2412 <duydang2412@gmail.com>
2022-06-12 01:57:03 +0700

    refactor(Program): call App.RegisterAccount()

--- Warehouse.Server/Program.Command.Register.cs
+++ Warehouse.Server/Program.Command.Register.cs
@@ -4,7 +4,7 @@ namespace Warehouse.Server;
 
 public partial class Program
 {
-	private static void RegisterCommand(ICommand sender, string arg)
+	private static async Task RegisterCommand(ICommand _, string arg)
 	{
 		var args = arg.Split(' ', 2);
 		if (args.Length != 2)
@@ -12,6 +12,11 @@ public partial class Program
 			Console.WriteLine("> register [username] [password]");
 			return;
 		}
-		Console.WriteLine($"An account with username {args[0]} has been registered");
+		if (await App.RegisterAccount(args[0], args[1]))
+		{
+			Console.WriteLine($"Account {args[0]} has been registered");
+			return;
+		}
+		Console.WriteLine($"Account {args[0]} was not registered");
 	}
 }
