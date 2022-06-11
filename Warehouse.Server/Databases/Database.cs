c079abf78846b7934d96e8de8d57aef35530feb6
duydang2412 <duydang2412@gmail.com>
2022-06-11 20:12:22 +0700

    feat(IDatabase): implement TryGetConnection methods

--- Warehouse.Server/Databases/Database.cs
+++ Warehouse.Server/Databases/Database.cs
@@ -10,10 +10,49 @@ public class Database : IDatabase
 		ConnectionStringBuilder = new SqlConnectionStringBuilder()
 		{
 			DataSource = "DUYDANG",
-			UserID = "warehouse_login_test",
 			InitialCatalog = "Warehouse",
 			TrustServerCertificate = true
 		};
+	}
+	public SqlConnection? TryGetConnection(IRoleAuth auth)
+	{
+		var connection = new SqlConnection(ConnectionStringBuilder.ToString());
+		try
+		{
+			connection.Open();
+			using var cmd = new SqlCommand("sp_setapprole", connection);
+			cmd.CommandType = System.Data.CommandType.StoredProcedure;
+			cmd.Parameters.AddWithValue("@rolename", auth.Name);
+			cmd.Parameters.AddWithValue("@rolePassword", auth.Password);
+			cmd.ExecuteNonQuery();
+			return connection;
+		}
+		catch
+		{
+			connection.Close();
+			connection.Dispose();
+			return default;
+		}
+	}
+	public async Task<SqlConnection?> TryGetConnectionAsync(IRoleAuth auth)
+	{
+		var connection = new SqlConnection(ConnectionStringBuilder.ToString());
+		try
+		{
+			await connection.OpenAsync();
+			using var cmd = new SqlCommand("sp_setapprole", connection);
+			cmd.CommandType = System.Data.CommandType.StoredProcedure;
+			cmd.Parameters.AddWithValue("@rolename", auth.Name);
+			cmd.Parameters.AddWithValue("@rolePassword", auth.Password);
+			await cmd.ExecuteNonQueryAsync();
+			return connection;
+		}
+		catch
+		{
+			await connection.CloseAsync();
+			connection.Dispose();
+			return default;
+		}
 	}
 }
 
--- Warehouse.Server/Databases/IDatabase.cs
+++ Warehouse.Server/Databases/IDatabase.cs
@@ -5,4 +5,6 @@ namespace Warehouse.Server.Databases;
 public interface IDatabase
 {
 	SqlConnectionStringBuilder ConnectionStringBuilder { get; }
+	SqlConnection? TryGetConnection(IRoleAuth auth);
+	Task<SqlConnection?> TryGetConnectionAsync(IRoleAuth auth);
 }
