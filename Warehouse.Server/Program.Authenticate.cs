db27a55e846f96df5c50547a0a4f7071397c2a3e
duydang2412 <duydang2412@gmail.com>
2022-06-11 14:48:17 +0700

    feat(Program): add Authenticate()

---
+++ Warehouse.Server/Program.Authenticate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+using Warehouse.Server.Databases;
+
+namespace Warehouse.Server;
+
+public partial class Program
+{
+	private static void Authenticate()
+	{
+		var database = Provider.GetRequiredService<IDatabase>();
+		Console.WriteLine("Enter password to login: ");
+		var password = Console.ReadLine();
+		database.ConnectionStringBuilder.Password = password;
+		try
+		{
+			using var connection = new SqlConnection(database.ConnectionStringBuilder.ToString());
+			connection.Open();
+		}
+		catch (SqlException)
+		{
+			Console.WriteLine("Login failed, exiting the program.");
+			Environment.Exit(0);
+		}
+		Console.WriteLine("Logged in succesfully");
+	}
+}
+
