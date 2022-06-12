336732ff3398843649da2cb927e82f015c1470c0
duydang2412 <duydang2412@gmail.com>
2022-06-12 13:55:26 +0700

    refactor(Application): RegisterAccount should throw in debug config

--- Warehouse.Server/Applications/Application.RegisterAccount.cs
+++ Warehouse.Server/Applications/Application.RegisterAccount.cs
@@ -6,18 +6,26 @@ public sealed partial class Application : IApplication
 {
 	public async Task<bool> RegisterAccount(string username, string password)
 	{
+		using var connection = await database.TryGetConnectionAsync(roleAuth);
+		if (connection is null)
+		{
+			return false;
+		}
+		using var cmd = new SqlCommand("usp_InsertStaffAccount", connection);
+		cmd.CommandType = System.Data.CommandType.StoredProcedure;
+		cmd.Parameters.AddWithValue("@username", username);
+		cmd.Parameters.AddWithValue("@password", password);
 		try
 		{
-			using var connection = await database.GetConnectionAsync(roleAuth);
-			using var cmd = new SqlCommand("usp_InsertStaffAccount", connection);
-			cmd.CommandType = System.Data.CommandType.StoredProcedure;
-			cmd.Parameters.AddWithValue("@username", username);
-			cmd.Parameters.AddWithValue("@password", password);
 			return await cmd.ExecuteNonQueryAsync() == 1;
 		}
 		catch
 		{
+#if DEBUG
+			throw;
+#else
 			return false;
+#endif
 		}
 	}
 }
