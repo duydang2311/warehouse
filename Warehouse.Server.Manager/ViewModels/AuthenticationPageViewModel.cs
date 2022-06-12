782176131e3b9c80b893ccb23020de6266db9322
duydang2412 <duydang2412@gmail.com>
2022-06-12 16:19:23 +0700

    feat(IAuthenticationPageViewModel): add IsEnabled property

--- Warehouse.Server.Manager/ViewModels/AuthenticationPageViewModel.cs
+++ Warehouse.Server.Manager/ViewModels/AuthenticationPageViewModel.cs
@@ -9,15 +9,24 @@ public class AuthenticationPageViewModel : ObservableObject, IAuthenticationPage
     private string password;
     private string usernameError;
     private string passwordError;
+    private bool isEnabled = false;
     public string Username
     {
         get => username;
-        set => SetProperty(ref username, value);
+        set
+        {
+            SetProperty(ref username, value);
+            UpdateIsEnabled();
+        }
     }
     public string Password
     {
         get => password;
-        set => SetProperty(ref password, value);
+        set
+        {
+            SetProperty(ref password, value);
+            UpdateIsEnabled();
+        }
     }
     public string UsernameError
     {
@@ -29,7 +38,33 @@ public class AuthenticationPageViewModel : ObservableObject, IAuthenticationPage
         get => passwordError;
         set => SetProperty(ref passwordError, value);
     }
+    public bool IsEnabled
+    {
+        get => isEnabled;
+        set => SetProperty(ref isEnabled, value);
+    }
+    public AuthenticationPageViewModel()
+    {
+        isEnabled = false;
+        username = password = usernameError = passwordError = "";
+    }
+    private void UpdateIsEnabled()
+    {
+        IsEnabled = true;
+        if (username.Length < 4
+        || password.Length < 8)
+        {
+            IsEnabled = false;
+            return;
+        }
+    }
     public void Login(object sender, RoutedEventArgs e)
     {
+        Username = username.Trim();
+        UpdateIsEnabled();
+        if (isEnabled)
+        {
+            // implement: send auth
+        }
     }
 }
