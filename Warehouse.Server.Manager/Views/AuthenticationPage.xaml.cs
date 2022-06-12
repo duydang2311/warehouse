1da96d689c08565c28d0a707fc5f5e18e3a7db9d
duydang2412 <duydang2412@gmail.com>
2022-06-12 15:49:23 +0700

    feat(AuthenticationPage): inject IAuthenticationPageViewModel

--- Warehouse.Server.Manager/Views/AuthenticationPage.xaml.cs
+++ Warehouse.Server.Manager/Views/AuthenticationPage.xaml.cs
@@ -1,31 +1,14 @@
-﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Microsoft.UI.Xaml.Controls;
+using Warehouse.Server.Manager.ViewModels;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace Warehouse.Server.Manager.Views;
 
-namespace Warehouse.Server.Manager.Views
+public sealed partial class AuthenticationPage : Page
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class AuthenticationPage : Page
+    public IAuthenticationPageViewModel ViewModel { get; }
+    public AuthenticationPage(IAuthenticationPageViewModel vm)
     {
-        public AuthenticationPage()
-        {
-            this.InitializeComponent();
-        }
+        InitializeComponent();
+        ViewModel = vm;
     }
 }
