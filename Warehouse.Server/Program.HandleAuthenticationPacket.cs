7da483e72c5bd45e454ae1e49369b56221dbaa36
duydang2412 <duydang2412@gmail.com>
2022-06-14 18:44:23 +0700

    feat(Program): handle and respond to authentication packet

--- Warehouse.Server/Program.HandleAuthenticationPacket.cs
+++ Warehouse.Server/Program.HandleAuthenticationPacket.cs
@@ -1,4 +1,5 @@
-﻿using Warehouse.Server.SocketHandlers;
+﻿using Microsoft.Data.SqlClient;
+using Warehouse.Server.SocketHandlers;
 using Warehouse.Shared.Packets;
 
 namespace Warehouse.Server;
@@ -7,6 +8,17 @@ public partial class Program
 {
 	private static async Task HandleAuthenticationPacket(ISocketHandler handler, IAuthenticationPacket packet)
 	{
+		using var connection = await Database.TryGetConnectionAsync(App.RoleAuth);
+		if (connection is null)
+		{
+			return;
+		}
+		using var cmd = new SqlCommand("udf_TryLoginToStaffAccount", connection);
+		cmd.CommandType = System.Data.CommandType.StoredProcedure;
+		cmd.Parameters.AddWithValue("@username", packet.Username);
+		cmd.Parameters.AddWithValue("@password", packet.Password);
+		var ok = (bool)(await cmd.ExecuteScalarAsync())!;
+		await handler.Send((await PacketSerializer.TrySerializeAsync(PacketFactory.GetAuthenticationResponsePacket(ok)))!);
 	}
 }
 
