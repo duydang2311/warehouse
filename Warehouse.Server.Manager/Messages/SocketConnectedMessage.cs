01958196bc1832de39b8b7b93297004ffbfbd90b
duydang2412 <duydang2412@gmail.com>
2022-06-14 16:14:37 +0700

    feat(Messages): add SocketConnectedMessage

---
+++ Warehouse.Server.Manager/Messages/SocketConnectedMessage.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Toolkit.Mvvm.Messaging.Messages;
+using Warehouse.Server.Manager.ViewModels;
+
+namespace Warehouse.Server.Manager.Messages;
+
+public class SocketConnectedMessage : ValueChangedMessage<object?>
+{
+    public SocketConnectedMessage() : base(null) { }
+}
