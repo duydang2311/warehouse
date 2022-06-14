86b9c9aa9c0ff9a14d96896f7e01dccc04dbb49f
duydang2412 <duydang2412@gmail.com>
2022-06-14 15:20:57 +0700

    feat(SocketConnection): add SocketConnectionPage, SocketConnectionViewModel

---
+++ Warehouse.Server.Manager/Views/SocketConnectionPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Warehouse.Server.Manager.Models.Sockets;
+using Warehouse.Server.Manager.ViewModels;
+
+namespace Warehouse.Server.Manager.Views;
+
+public sealed partial class SocketConnectionPage : Page
+{
+    private readonly ISocketConnectionViewModel ViewModel;
+    public SocketConnectionPage(ISocketConnectionViewModel vm)
+    {
+        this.InitializeComponent();
+        DataContext = ViewModel = vm;
+    }
+}
